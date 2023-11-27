07247f67db02d4639ea86390eedf10707ebbdc3b
Aftermath001 <alvinasiachi@gmail.com>
2023-11-28 00:32:58 +0300

    Pages and Routes

---
+++ Frontend/rabbi-co/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+   <div>
+    <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      
+    
+   </div>
+  );
+}
+
+export default App;
